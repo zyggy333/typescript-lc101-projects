f6fcb1ee7c768431804227d71c7f139d139eacf1
Zyggy Buss <zyggy333@gmail.com>
2019-10-17 20:14:49 -0500

    Undefined is stupid

---
+++ studio/Astronaut.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Astronaut = /** @class */ (function () {
+    function Astronaut(massKg, name) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+    return Astronaut;
+}());
+exports.Astronaut = Astronaut;
---
+++ studio/Rocket.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sumWeight = 0;
+        for (var i = 0; i < items.length; i++) {
+            sumWeight = sumWeight + items[i].massKg;
+        }
+        return sumWeight;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
